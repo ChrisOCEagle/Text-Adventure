7fe22222a6c4d6840bdc14dd0c66730ed446c20f
ChrisOCEagle <christopherdavyydoconnell@yahoo.com>
2019-09-09 08:47:12 -0400

    made some changes to the readme

---
+++ terminal.js
@@ -0,0 +1,18 @@
+/* Here is a list of all the readline methods:
+    
+    Interface - Object,
+        super_ - Object,
+            EventEmitter - recursive method AKA function,
+            usingDomains - boolean,
+            defaultMaxListeners - number,
+            init - method AKA function,
+            listenerCount - method AKA function,
+    clearLine - method AKA function,
+    clearScreenDown - method AKA function,
+    createInterface - method AKA function,
+    cursorTo - method AKA function,
+    emitKeyPress - method AKA function,
+    moveCursor - method AKA function,
+
+Require the readline node application to be able to read data from a readable stream one line at a time */
+const readline = require('readline');
